e6244374b7236fbcacc99ed693ab72646febf510
Achraf <ninjagoo147@outlook.com>
2020-04-17 20:04:00 +0100

    Final Work

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Quotes
+# Quote Manager Application
+
+A Quote Manager Application (using nodejs , expressjs, mysql, ejs).
+
+## What You can do with this Application
+
+with this application you can : Add - Edit and Delete a Quote.
+
+## Installation
+
+
+```bash
+npm install
+```
+ Change Your Database Root password To : 1234
+
+## Usage
+
+1.Add quotes ( '/' or add button )
+
+2.Delete quotes ( '/delete/(id)' or delete button )
+
+3.Update quotes ( '/update/(id)' or update button )
+
+4.Display quotes ( '/quotes' )
+
+5.Display specific quote ( '/quote/(id)' )
+
+## ✔️ Youcode ✔️
---
+++ app.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+ 
+//replace here with your DB 
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    database:'quotes'
+});
+ 
+connection.connect(function(error){
+    if(!!error) console.log(error);
+    else console.log('Database Connected!');
+}); 
+ 
+//set views file
+app.set('views',path.join(__dirname,'views'));
+
+//my css
+app.use('/public', express.static('public'));
+			
+//set view engine
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+ 
+ 
+app.get('/',(req, res) => {
+    
+    let sql = "SELECT * FROM users";
+    let query = connection.query(sql, (err, rows) => {
+        if(err) throw err;
+        res.render('user_index', {
+            title : 'Enter Your Quote',
+            users : rows
+        });
+    });
+});
+ 
+ 
+app.get('/add',(req, res) => {
+    res.render('user_add', {
+        title : 'Enter Your Quote'
+    });
+});
+ 
+app.post('/save',(req, res) => { 
+    let data = {name: req.body.name, email: req.body.email, quote: req.body.quote};
+    let sql = "INSERT INTO users SET ?";
+    let query = connection.query(sql, data,(err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+});
+ 
+app.get('/edit/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `Select * from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.render('user_edit', {
+            title : 'Enter Your Quote',
+            user : result[0]
+        });
+    });
+});
+ 
+ 
+app.post('/update',(req, res) => {
+    const userId = req.body.id;
+    let sql = "update users SET name='"+req.body.name+"',  email='"+req.body.email+"',  quote='"+req.body.quote+"' where id ="+userId;
+    let query = connection.query(sql,(err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+});
+ 
+ 
+app.get('/delete/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `DELETE from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+ 
+ 
+// Server Listening
+app.listen(3000, () => {
+    console.log('Server is running at port 3000');
+});
+ 
